e9708188a49105fe005d47c303975e6c3151f9b3
Kthim Imeri <kthim_imeri@outlook.com>
2024-09-01 22:24:56 +0200

    AssetData

---
+++ src/common/AssetData.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from dataclasses import dataclass
+
+@dataclass
+class AssetData:
+    ticker: str
+    shareprice: pd.DataFrame
+    volume: pd.DataFrame
+    dividends: pd.DataFrame
+    genInfo: dict               #todo: stock_info.info
+    financials: pd.DataFrame    #todo: try getting quarterly ones. And drop most columns
