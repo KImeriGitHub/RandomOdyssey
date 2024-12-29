5339545cedf37fd9a68c4eef8d9b81b4cb9cb7ff
kimeri <kthim_imeri@outlook.com>
2024-12-29 21:03:34 +0100

    fix FeatureFinancialData

--- src/featureAlchemy/FeatureFinancialData.py
+++ src/featureAlchemy/FeatureFinancialData.py
@@ -46,7 +46,6 @@ class FeatureFinancialData():
     
     catav_ann = [
         'fiscalDateEnding',
-        'reportedDate',
         'reportedEPS',
         'grossProfit',
         'totalRevenue',
